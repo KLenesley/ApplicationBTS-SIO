fb1b93974158521a0f295b699c1e6f4e7bfcea6f
BEAUVERGER Evan <beaueva@ndlp.fr>
2025-03-31 12:51:53 +0200

    ajout menu

---
+++ src/components/Menu.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Menu() {
+
+    return (
+        <div>
+            <a href="http://localhost:3000/">Accueil </a> 
+            <a href="http://localhost:3000/specialites">Spécialités </a> 
+            <a href="http://localhost:3000/certifications">Certification</a>
+        </div>
+    );
+
+}
