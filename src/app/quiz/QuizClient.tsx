39b7f23c58f8fb00166607a9bf2db6b999bfb3d2
KLenesley <lenesley.kylian@ik.me>
2025-05-12 11:09:02 +0200

    temp

--- src/app/quiz/QuizClient.tsx
+++ src/app/quiz/QuizClient.tsx
@@ -45,6 +45,7 @@ export default function QuizClient({ quiz_questions, quiz_answers }) {
               </div>
             )
           ));
+
         } else {
           return (
             <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
@@ -58,6 +59,7 @@ export default function QuizClient({ quiz_questions, quiz_answers }) {
                   </li>
                 ))}
               </ul>
+              
               <button onClick={handleRestart} className="mt-6 px-5 py-2 bg-purple-600 text-white font-medium rounded-lg hover:bg-purple-700 transition">
                 Recommencer
               </button>
